3b10b047cae1db34b47041448c4cae236fcf3032
William Eduardo Meza <williameduardomeza@gmail.com>
2020-05-23 20:55:47 -0500

    feat: init week 5

---
+++ lesson5/js/last-modified.js
@@ -0,0 +1,41 @@
+/* 
+let lastModified = new Date(document.lastModified);
+let fullDate = lastModified.toLocaleString('en-US', {month: "2-digit", day: "2-digit", year: "numeric"});
+let time = lastModified.toLocaleString('en-GB', {hour: "2-digit", minute: "2-digit", second: "2-digit"});
+let dateTime = `Last Updated: ${fullDate} ${time}`;
+document.getElementById("lastModified").innerHTML = dateTime;
+ */
+
+let dayNames = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+];
+
+let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+];
+
+let curDate = new Date();
+let dayName = dayNames[curDate.getDay()];
+let monthName = months[curDate.getMonth()];
+let year = curDate.getFullYear();
+let currentDate = `${dayName}, ${curDate.getDate()}, ${monthName} ${year}`;
+
+document.getElementById("currentDate").innerHTML = currentDate;
+  
