26a580caee3b5b347e070ef5fdcbbecec6b2a3d9
William Eduardo Meza <williameduardomeza@gmail.com>
2020-07-05 22:57:26 -0500

    feat: add API fetch for incoming events

--- lesson11/js/forecast.js
+++ lesson11/js/forecast.js
@@ -40,3 +40,22 @@ fetch(forecastURL)
         }
         
 });
+
+const requestURL = "https://byui-cit230.github.io/weather/data/towndata.json";
+fetch(requestURL)
+  .then(function (response) {
+      return response.json();
+  })
+  .then(function (jsonObject) {
+      const towns = jsonObject['towns'];
+      const townName = towns.filter(x => x.name.includes("Preston"));
+      console.log(townName)
+      const events = townName[0].events;
+
+      events.forEach(event => {
+          let party = document.createElement('p');
+          party.innerText = event
+          let eventInfo = document.getElementById('town-events');
+          eventInfo.appendChild(party)
+      })
+  });
